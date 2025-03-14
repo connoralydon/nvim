715f3afae759d2cfa33a8dd852013d99671a3926
Connor Lydon <clydon@anduril.com>
2025-03-14 15:58:34 -0700

    udpated rainbow

--- lua/plugins/indent-rainbow.lua
+++ lua/plugins/indent-rainbow.lua
@@ -5,7 +5,7 @@ return {
     -- Other blankline configuration here
     return require("indent-rainbowline").make_opts(opts, {
       -- How transparent should the rainbow colors be. 1 is completely opaque, 0 is invisible. 0.07 by default
-      color_transparency = 0.07,
+      color_transparency = 0.05,
       -- The 24-bit colors to mix with the background. Specified in hex.
       -- { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec, } by default
       -- colors = { 0xff0000, 0x00ff00, 0x0000ff },
