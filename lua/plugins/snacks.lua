59252dcc8fad20b16de37b5c1726590fd20a2ed8
Connor Lydon <clydon@anduril.com>
2025-03-14 16:14:31 -0700

    added snacks indent

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,26 +1,31 @@
 return {
-  {
-    "folke/snacks.nvim",
-    -- priority = 1000,
-    -- lazy = false,
-    opts = function()
-      ---@type snacks.Config
-      return {
-        -- notifier = { enabled = true },
-        -- quickfile = { enabled = true },
-        -- bigfile = { enabled = true },
-        -- words = { enabled = true },
-        -- toggle = { map = LazyVim.safe_keymap_set },
-        -- statuscolumn = { enabled = false }, -- we set this in options.lua
-        terminal = {
-          win = {
-            position = "float",
-            style = "terminal",
-            interactive = true,
-            auto_insert = true,
-          },
-        },
-      }
-    end,
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    -- explorer = { enabled = true }, -- there is adefault with lazyvim
+    indent = { enabled = true, animate = { enabled = false } },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    terminal = {
+      win = {
+        position = "float",
+        style = "terminal",
+        interactive = true,
+        auto_insert = true,
+      },
+    },
   },
 }
