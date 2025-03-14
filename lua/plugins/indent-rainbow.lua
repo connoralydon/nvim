cac9870ac87847b32f25f5fcfd5f5aea77dbf9ed
Connor Lydon <clydon@anduril.com>
2025-03-14 15:59:57 -0700

    udpated rainbow

--- lua/plugins/indent-rainbow.lua
+++ lua/plugins/indent-rainbow.lua
@@ -7,7 +7,7 @@ return {
       -- How transparent should the rainbow colors be. 1 is completely opaque, 0 is invisible. 0.07 by default
       color_transparency = 0.05,
       -- The 24-bit colors to mix with the background. Specified in hex.
-      -- { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec, } by default
+      { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec },
       -- colors = { 0xff0000, 0x00ff00, 0x0000ff },
     })
   end,
