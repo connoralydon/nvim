1050e876f0b2366e11548778d923f59e2d4076c6
Connor Lydon <clydon@anduril.com>
2025-03-14 16:07:01 -0700

    udpated rainbow

--- lua/plugins/indent-rainbow.lua
+++ lua/plugins/indent-rainbow.lua
@@ -3,13 +3,13 @@ return {
   main = "ibl",
   opts = function(_, opts)
     -- Other blankline configuration here
-    return require("indent-rainbowline").make_opts(opts, {
+    return require("indent-rainbowline").make_opts(opts({
       -- How transparent should the rainbow colors be. 1 is completely opaque, 0 is invisible. 0.07 by default
-      color_transparency = 0.05,
+      color_transparency = 0.2,
       -- The 24-bit colors to mix with the background. Specified in hex.
-      { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec },
+      { 0xA4036F, 0x048BA8, 0xEFEA5A },
       -- colors = { 0xff0000, 0x00ff00, 0x0000ff },
-    })
+    }))
   end,
   dependencies = {
     "TheGLander/indent-rainbowline.nvim",
