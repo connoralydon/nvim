c0a776d8187d5d4d81d8d8b7f4aec9390e778974
Connor Lydon <clydon@anduril.com>
2025-03-14 17:56:10 -0700

    pushed change

--- flake.nix
+++ flake.nix
@@ -23,6 +23,21 @@
             mkdir -p $out
             cp -r ./* $out/
             find $out -type f -not -path '*/.git/*' -print0 | sort -z | xargs -0 sha256sum | sha256sum > $out/config.hash
+            
+            mkdir -p ~/.config/nvim
+            if [ -f ~/.config/nvim/config.hash ]; then
+              if diff -q $out/config.hash ~/.config/nvim/config.hash >/dev/null; then
+                echo "Neovim configuration is up to date"
+              else
+                rsync -av --delete --exclude='.git' $out/ ~/.config/nvim/
+                cp $out/config.hash ~/.config/nvim/config.hash
+                echo "Neovim configuration has been updated in ~/.config/nvim"
+              fi
+            else
+              rsync -av --delete --exclude='.git' $out/ ~/.config/nvim/
+              cp $out/config.hash ~/.config/nvim/config.hash
+              echo "Neovim configuration has been initialized in ~/.config/nvim"
+            fi
           '';
         };
       in
@@ -39,23 +54,6 @@
           ];
           shellHook = ''
             mkdir -p ~/.config/nvim
-            if [ -f ~/.config/nvim/config.hash ]; then
-              echo ${nvim-config}
-              cat ${nvim-config}/config.hash 
-              echo ~/.config/nvim/config.hash
-              cat ~/.config/nvim/config.hash
-              if diff -q ${nvim-config}/config.hash ~/.config/nvim/config.hash >/dev/null; then
-                echo "Neovim configuration is up to date"
-              else
-                rsync -av --delete --exclude='.git' ${nvim-config}/ ~/.config/nvim/
-                cp ${nvim-config}/config.hash ~/.config/nvim/config.hash
-                echo "updatedddd"
-                echo "Neovim configuration has been updated in ~/.config/nvim"
-              fi
-            else
-              rsync -av --delete ${nvim-config}/ ~/.config/nvim/
-              echo "Neovim configuration has been initialized in ~/.config/nvim"
-            fi
           '';
         };
       }
