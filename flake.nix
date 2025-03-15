15425bd567f4bbb5a4f90e2c15947d7007f38a26
Connor Lydon <clydon@anduril.com>
2025-03-14 17:57:34 -0700

    pushed change

--- flake.nix
+++ flake.nix
@@ -22,22 +22,6 @@
           installPhase = ''
             mkdir -p $out
             cp -r ./* $out/
-            find $out -type f -not -path '*/.git/*' -print0 | sort -z | xargs -0 sha256sum | sha256sum > $out/config.hash
-            
-            mkdir -p ~/.config/nvim
-            if [ -f ~/.config/nvim/config.hash ]; then
-              if diff -q $out/config.hash ~/.config/nvim/config.hash >/dev/null; then
-                echo "Neovim configuration is up to date"
-              else
-                rsync -av --delete --exclude='.git' $out/ ~/.config/nvim/
-                cp $out/config.hash ~/.config/nvim/config.hash
-                echo "Neovim configuration has been updated in ~/.config/nvim"
-              fi
-            else
-              rsync -av --delete --exclude='.git' $out/ ~/.config/nvim/
-              cp $out/config.hash ~/.config/nvim/config.hash
-              echo "Neovim configuration has been initialized in ~/.config/nvim"
-            fi
           '';
         };
       in
@@ -54,6 +38,8 @@
           ];
           shellHook = ''
             mkdir -p ~/.config/nvim
+            rsync -av --delete ${nvim-config}/ ~/.config/nvim/
+            echo "Neovim configuration has been updated in ~/.config/nvim"
           '';
         };
       }
